f61aa09bf0a3f50f2a3d20b611ac474948e66194
FOF-T4SU <fof.t4su.sayna@gmail.com>
2022-07-17 23:35:20 +0000

    debut du script

--- Script/game/game.js
+++ Script/game/game.js
@@ -1,33 +1,12 @@
-// $(document).ready(function() {
-//     $.ajax({
-//         url: "https://octopus-app-2u6og.ondigitalocean.app/questions/all",
-//         dataType: "json",
-//         succes: function(questions) {
-//             console.log(questions);
-//         },
-//         error: function() {
-//             console.log("impossible de se connecter");
-//         },
-//     });
-//     // console.log("good");
-// });
-
-// const xhttp = new XMLHttpRequest();
-// xhttp.onload = function(question) {
-//     console.log(question.currentTarget.response);
-// };
-
-// xhttp.open(
-//     "GET",
-//     "https://octopus-app-2u6og.ondigitalocean.app/questions/all",
-//     true
-// );
-// xhttp.send();
-
-fetch("https://octopus-app-2u6og.ondigitalocean.app/questions/all")
-    .then(function(question) {
-        console.log(question.json(Promise));
-    })
-    .then(function(result) {
-        console.log(result);
+$(document).ready(function() {
+    $.ajax({
+        url: "https://octopus-app-2u6og.ondigitalocean.app/questions/all",
+        datatype: "json",
+        success: function(questions) {
+            console.log(questions);
+        },
+        error: function() {
+            console.log("impossible de se connecter");
+        },
     });
+});
